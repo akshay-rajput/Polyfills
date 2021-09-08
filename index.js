c4d4f16136e7a17a8e0a207beccd4bb615586984
Akshay <25450571+akshay-rajput@users.noreply.github.com>
2021-09-08 07:13:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// filter
+/*
+- filter() calls a provided callbackFn function once for each element in an array, and constructs a new array of all the values for which callbackFn returns a value that coerces to true.
+- callbackFn is invoked only for indexes of the array which have assigned values; it is not invoked for indexes which have been deleted or which have never been assigned values.
+- filter() does not mutate the array on which it is called.
+ */
+
+
+
+// forEach
+/*
+*/
+
+Array.prototype.eachElm = function(callback) {
+  // callback here is the callback function
+  // which actual .forEach() function accepts
+  for (var i = 0; i < this.length; i++) {
+    callback(this[i], i, this) // currentValue, index, array
+  }
+}
+
+
+// .map()
+/*
+.map() is used to iterate over the array, and do some manipulation in it with the items and then after doing that so, returns the manipulated array.
+*/
+Array.prototype.ourMap = function(callback) {
+  var arr = [] // since, we need to return an array
+  for (var i = 0; i < this.length; i++) {
+    arr.push(callback(this[i], i, this)) // pushing currentValue, index, array
+  }
+  return arr; // finally returning the array
+}
