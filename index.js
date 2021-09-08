712bdd78d91b5660da003a195ec474ddbd26f44e
Akshay <25450571+akshay-rajput@users.noreply.github.com>
2021-09-08 12:53:56 +0000

    Add polyfill for reduce()

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ Array.prototype.customFilter = function(callback){
 
 
 // forEach
-/*
-*/
-
 Array.prototype.eachElm = function(callback) {
   // callback here is the callback function
   // which actual .forEach() function accepts
@@ -48,7 +45,72 @@ Array.prototype.ourMap = function(callback) {
   return arr; // finally returning the array
 }
 
-let arr = [1,3,4,6,7,8];
+// find()
+/*
+  returns the value of first element in the array that satisfies the provided testing function.
+*/
+Array.prototype.customFind = function(testingFunc){
+  if(typeof testingFunc !== "function"){
+    throw new TypeError("customFind needs a callback function");
+  }
+
+  for(let i = 0; i<this.length; i++){
+    if(testingFunc(this[i], i, this)){
+      return this[i];
+    }
+  }
+  return undefined;
+}
+
+//reduce()
+/*
+  - take a reducer callback function and execute it on each element of array
+  - pass the return value from calculation on preceding element.
+  - final result is a single value
+*/
+Array.prototype.customReduce = function(reducerfunc, initialValue){
+  let currentValue = null;
+  let acc = 0;
+
+  // if array length less than 1
+  if(this.length < 1){
+    throw new Error("Cannot reduce an empty array");
+  }
+
+  // if initialvalue not provided
+  if(initialValue === undefined){
+    acc = this[0]; // set first element as accumulator then
+  }else{
+    acc = initialValue;
+  }
+
+  for(let i = 0; i < this.length; i++){
+    if(this[i] !== null && this[i] !== undefined){
+      currentValue = reducerfunc(acc, this[i], i, this);
+      acc = currentValue;
+    }
+  }
+  return acc;
+}
+
+
+let arr = [1,3,4,6,7,,8];
+
 console.log("OG array: ", arr + "\n");
-console.log("Squares: ", arr.ourMap(item=> item * item));
-console.log("only odds: ", arr.customFilter(item => item % 2 === 1));
+console.log("Map: Squares: ", arr.ourMap(item=> item * item));
+console.log("Filter: only odds: ", arr.customFilter(item => item % 2 === 1));
+
+console.log("find 1st even: ", arr.customFind(item => item % 2 === 0));
+
+let reducer = (acc, current) => acc+current;
+
+console.log("Reduce: sum: "+ arr.customReduce(reducer, 0));
+
+
+// let arr2 = [1,2,3];
+// let reducer2 = (acc, current) => {
+//   console.log("acc: ", acc + " -- current: ", current);
+//   return acc + current**2;
+// }
+// arr2.customReduce(reducer2, 1);
+// arr.reduce(reducer, 1);
